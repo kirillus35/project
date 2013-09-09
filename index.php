c3af0fdbdd92d28b617b483a7796f185e3b45cce
Kuznetsov Kirill <kuziy96@mail.ru>
2013-09-09 22:13:10 +0400

    first commit

---
+++ index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">	
+	<title>Награды</title>
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="main.css" rel="stylesheet">
+</head>
+<body>
+<div class="navbar navbar-inverse navbar-fixed-top">
+	<div class="navbar-inner">
+		<div class="container-fluid">
+			<button class="btn btn-navbar" data-target=".nav-collapse" data-toggle="collapse" type="button">
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<a class="brand" href="">PROJECT</a>
+			<div class="nav-collapse">
+				<ul class="nav">
+					<li class="active"><a href="">Главная</a></li>
+					<li><a href="#about">Учителя</a></li>
+					<li><a href="#contact">Ученики</a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="container">
+	<div class="row-fluid">
+	    <div class="span12">
+	  	    
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pulvinar elit dui, aliquam aliquam tellus gravida et. Vivamus malesuada arcu eget congue mollis. Sed congue urna vehicula rhoncus eleifend. Vivamus consectetur lobortis dui in viverra. Nulla at lorem nulla. Maecenas varius semper mi, vel tristique metus molestie ut. Ut non felis semper, viverra mauris a, eleifend neque. Duis congue lacus sit amet quam dapibus tincidunt. Suspendisse nibh nisi, suscipit non nisi non, auctor consequat est. Donec eget pharetra lectus. Fusce feugiat ullamcorper risus id vehicula. Donec eget orci accumsan, euismod diam eu, aliquam enim. Integer vel placerat tellus, sit amet dapibus purus. Etiam id neque imperdiet, ultricies erat sit amet, rhoncus eros.
+
+Nulla sodales metus ac justo luctus, et porttitor turpis mollis. Ut eu convallis dui. Sed convallis, odio et volutpat placerat, felis mauris semper nunc, vitae volutpat odio ligula vel sem. Etiam vestibulum, nisl in sodales convallis, turpis lacus condimentum nunc, non tincidunt odio dolor id tortor. Nunc vehicula odio eget rhoncus suscipit. Integer sed lectus nibh. Mauris rutrum, libero in posuere ultrices, leo dui vestibulum mauris, nec eleifend elit leo eu diam. Integer iaculis sapien lectus. Ut eleifend risus eu nibh vehicula tincidunt. Aliquam nec tincidunt nisi, in tempor mauris. Etiam ornare a ligula sed posuere. Donec ac elementum tellus. Proin pellentesque vel urna eget condimentum. In id sem euismod, tristique ipsum ut, bibendum odio. Duis suscipit dui augue, sed tempor nulla lacinia ac.
+
+Mauris sagittis malesuada metus. Suspendisse nibh dui, viverra at euismod in, bibendum a lectus. Aliquam hendrerit magna erat, et dictum leo ullamcorper vel. Nam ultrices sagittis pretium. Etiam sit amet turpis dui. Donec eu tortor eget purus cursus venenatis. Cras nec pretium eros. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vivamus mauris ipsum, dictum ut ipsum a, imperdiet sagittis urna. Mauris pretium, turpis in luctus euismod, urna ante hendrerit augue, et fermentum libero purus nec urna. Quisque consectetur diam vehicula ipsum varius, sed rhoncus diam rhoncus. Fusce vel orci at leo interdum auctor nec eu libero. Donec tempor condimentum velit, a luctus ipsum accumsan at. Fusce vel nunc massa. Sed non tempor dolor. Suspendisse vel est non lacus congue pretium.
+
+Vestibulum quis cursus mi, nec ultrices sem. Nulla urna felis, scelerisque luctus lobortis quis, dictum non quam. Sed ac cursus lacus, in pellentesque enim. In hac habitasse platea dictumst. Vivamus dignissim, nunc ut semper faucibus, purus enim tempus erat, in luctus lectus felis vel justo. Nunc tempor accumsan ipsum, at auctor sapien. Nam auctor orci tellus. Maecenas sit amet tortor risus. Pellentesque fermentum aliquam magna, vitae consectetur magna. Sed pellentesque, nisi nec sodales molestie, dui tortor malesuada sapien, vehicula dapibus elit enim in nisl. Mauris placerat elit eget felis iaculis sagittis. Integer risus velit, volutpat a lacinia non, molestie eget ante. Nam sed tristique elit, at eleifend risus. Vivamus dapibus porta diam cursus commodo. Integer eu accumsan diam, a aliquam ante.
+
+Donec dapibus sem est, non facilisis libero aliquet non. Proin congue dolor eu commodo consequat. Ut sodales augue felis, vel bibendum libero suscipit et. Etiam sed mi eget arcu adipiscing tristique. Quisque imperdiet dui ut viverra ultrices. Maecenas id lacinia nunc, sit amet ultrices nisi. Pellentesque pretium magna sit amet lorem pharetra tempor at sit amet ante. Nunc porta vel purus posuere bibendum. Phasellus id varius diam. Sed dictum sit amet lorem et auctor. Nullam ut elementum turpis. Nam eget eros et velit sagittis pharetra. Duis non porttitor enim.
+
+Maecenas erat lacus, adipiscing mattis libero vel, volutpat gravida augue. Praesent bibendum sagittis ante id cursus. Ut accumsan varius turpis ac consequat. Nullam condimentum, nisi sed mattis tempus, lectus risus tincidunt metus, id dignissim felis turpis ac leo. Cras iaculis dui quis posuere varius. Mauris at mauris dapibus, laoreet orci volutpat, porttitor nisl. Quisque erat odio, convallis vitae varius sed, mattis id lectus. Fusce in felis ante. Fusce lobortis molestie nisi quis sollicitudin. Nam ut nulla ac libero tincidunt laoreet eget et ante. Etiam tortor lorem, semper sed elit eget, aliquam laoreet metus. Mauris luctus mauris erat, quis laoreet enim ultrices ut. Aliquam placerat ante ac dignissim imperdiet. Aenean scelerisque blandit elit, id porta ligula imperdiet quis.
+
+Donec varius vitae tellus quis cursus. Proin sit amet volutpat dolor, eget tempor tortor. Suspendisse imperdiet ipsum ac lacus iaculis cursus. Nunc tempor eget leo nec elementum. Aliquam velit ipsum, euismod eu volutpat vel, interdum non eros. Integer lorem metus, vulputate non bibendum eu, scelerisque a neque. Proin elementum, turpis ut aliquet vehicula, velit nisl dignissim lacus, id tincidunt nunc sapien at erat. Vivamus non tristique lorem. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vehicula dolor eget consectetur congue.
+
+Duis sodales augue sodales, hendrerit lorem a, semper purus. Donec mattis ultricies quam. Donec rutrum, neque et vulputate dictum, augue ante egestas dui, nec pellentesque nisi justo ac lacus. Quisque vitae leo lobortis, semper eros vel, malesuada tortor. Donec congue id quam at gravida. Pellentesque varius diam purus, a posuere sem condimentum ut. Morbi et felis nisi. Nunc sed ornare purus.
+
+Phasellus in tortor at metus lacinia pulvinar. Nulla facilisi. Duis quis imperdiet libero. Nullam in pharetra eros. Aenean vestibulum dui nec velit porttitor, vitae dictum risus hendrerit. In mattis metus quis nibh iaculis egestas. Donec rutrum est in risus blandit, ut cursus nisl sodales. Cras tincidunt ipsum a vestibulum ullamcorper. Integer vehicula lorem lectus, at blandit erat iaculis ac. Vestibulum eu felis in sapien semper pharetra a a quam. Vestibulum ultricies rutrum nibh vel sollicitudin. Morbi convallis sed mauris vel mattis. Vestibulum blandit egestas enim, ut gravida quam imperdiet in.
+
+Maecenas tempor id justo ac lobortis. Fusce sapien tellus, luctus id sapien sed, dignissim placerat eros. Nulla nec nibh nisi. Nam ut pharetra erat. Praesent vitae massa eu nisi tincidunt lobortis. Donec feugiat accumsan lectus, ut elementum ipsum ultricies nec. Cras ante ipsum, hendrerit eget lectus sit amet, aliquet elementum ligula. Sed tincidunt neque at urna dapibus, in pellentesque lectus tempus. Proin eget nibh vitae massa ultrices tincidunt. Nunc non ornare est. Phasellus nunc purus, lobortis vitae diam in, imperdiet luctus erat. 
+		    <div class="row-fluid">
+		    <div class="span6">Level 2</div>
+		    <div class="span6">Level 2</div>
+	    	</div>
+	    </div>
+	</div>
+</div>	
+
+<div class="footer">
+	&copy; 2013 <a href="https://vk.com/kirillus35">Кузнецов Кирилл</a>
+</div>
+	<script src="http://code.jquery.com/jquery-latest.js"></script>
+	<script src="js/bootstrap.min.js" type="text/javascript"></script>
+	<script type="text/javascript" src="jquery.min.js"></script>	
+</body>
+</html>
